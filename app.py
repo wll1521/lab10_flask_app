40296bcfd1064e7a994540cf965a32b8a2486664
wll1521 <wll1521@jagmail.southalabama.edu>
2024-05-22 10:34:26 -0500

    initial update

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import psycopg2
+
+#RDS connection parameters
+ENDPOINT= "database-1.cfgug4m84026.us-east-1.rds.amazonaws.com"
+PORT= "5432"
+USER= "postgres"
+PASSWORD = "Student1"
+REGION= "us-east-1"
+DBNAME= "postgres"
+
+app=Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return "index"
+
+@app.route('/students')
+def get_students():
+    #gets the credentials from .aws/credentials
+    try:
+        conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD)
+        cur = conn.cursor()
+        cur.execute("""SELECT * from test""")
+        query_results = cur.fetchall()
+        print(query_results)
+        return(query_results)
+    except Exception as e:
+        print("Database connection failed due to {}".format(e)) 
+        return("Database connection failed due to {}".format(e))
+
+if __name__ == "__main__":
+    app.run(debug=True)
